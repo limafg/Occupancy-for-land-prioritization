f43e3aa2f0b46b20a470d509279c9020cbcc69ab
limafg <glimafilipe@gmail.com>
2024-06-25 18:10:26 -0300

    updating script for 3 species

--- 2_Occupancy-modeling.R
+++ 2_Occupancy-modeling.R
@@ -1,1 +1,13 @@
 # Occupancy models
+
+occupancy <- "model{
+for(k in 1:length(data[,,k])){
+for(i in 1:nrow){
+for(j in 1:ncol){
+n[i,j,k]~dbin(theta[k]*Z[i,k], 1)
+}
+Z[i,k]~dbin(psi[k], 1)
+}
+}
+
+}"
